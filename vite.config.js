402db0dee441b186b86ae8913e033d371a675445
mpmathis01 <mpmathis01@gmail.com>
2026-01-08 04:51:05 -0300

    chore: enable pwa versioning timestamp

--- vite.config.js
+++ vite.config.js
@@ -36,6 +36,9 @@ export default defineConfig(({ mode }) => {
         },
         workbox: {
           inlineWorkboxRuntime: true,
+          additionalManifestEntries: [
+            { url: 'index.html', revision: `${Date.now()}` }
+          ]
         }
       })
     ],
